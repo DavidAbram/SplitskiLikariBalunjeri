845ac01fa9a00b9e25ac1b0fd5e4bc4fa8138b45
DavidAbram <david@abram.com>
2020-05-12 11:58:01 +0200

    added contact page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,6 +4,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 import IndexPagePreview from './preview-templates/IndexPagePreview';
 import MembersPagePreview from './preview-templates/MembersPagePreview';
+import ContactPagePreview from './preview-templates/ContactPagePreview';
 import VisionPagePreview from './preview-templates/VisionPagePreview';
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import NotificationsPreview from './preview-templates/NotificationsPreview';
@@ -18,6 +19,7 @@ CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('members', MembersPagePreview);
 CMS.registerPreviewTemplate('vision', VisionPagePreview);
 CMS.registerPreviewTemplate('about', AboutPagePreview);
+CMS.registerPreviewTemplate('contact', ContactPagePreview);
 CMS.registerPreviewTemplate('notifications', NotificationsPreview);
 CMS.registerPreviewTemplate('activities', ActivitiesPreview);
 CMS.registerPreviewTemplate('header', HeaderPreview);
---
+++ src/templates/contact-page.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout';
+import Title from '../components/Title';
+import Content, { HTMLContent } from '../components/Content';
+
+export const ContactPageTemplate = ({
+  title,
+  content,
+  PageContent = Content,
+}) => (
+  <>
+    <Title>{title}</Title>
+    <PageContent>{content}</PageContent>
+  </>
+)
+
+ContactPageTemplate.propTypes = {
+  title: PropTypes.string,
+  content: PropTypes.string,
+}
+
+const ContactPage = ({ data }) => {
+  const { frontmatter, html } = data.markdownRemark;
+  return (
+    <Layout>
+      <ContactPageTemplate
+        title={frontmatter.title}
+        content={html}
+        PageContent={HTMLContent}
+      />
+    </Layout>
+  )
+}
+
+ContactPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      html: PropTypes.string,
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default ContactPage
+
+export const pageQuery = graphql`
+  query ContactPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "contact-page" } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
