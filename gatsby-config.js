dc76316f70d700978badfdb0fbacfdd9c7a21d91
DavidAbram <david@abram.com>
2020-05-16 01:19:34 +0200

    fixed ci

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,7 @@ module.exports = {
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
